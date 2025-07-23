c9ca4e9f30db0fff97db4c9bc57d2c21bcc21a46
MATheusc4 <74937108+C4mat@users.noreply.github.com>
2025-07-23 20:38:09 -0300

    Create entrada_de_dados_f_strings.py

---
+++ Primeiros_Passos/entrada_de_dados_f_strings.py
@@ -0,0 +1,10 @@
+# entrada de dados - receber dados de usuario
+# input()
+# exemplo em uma variavel
+nome = input("digite seu nome: ")
+# usando input pra receber informação digitada de um usuario
+
+print(nome)  # exibindo o valor digitado
+
+# tambem é possivel fazer a utilização desse valor ja digitado em outro print nesse caso depois a string colocar uma , e o nome da varivel
+print("bem vindo", nome)
