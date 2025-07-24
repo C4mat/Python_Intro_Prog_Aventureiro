f0f5429d18b2d18dd700caebefd0a1ffe5aeb756
MATheusc4 <74937108+C4mat@users.noreply.github.com>
2025-07-24 20:24:35 -0300

    FStrings
    Conceitto de Fstrings - Output de fstrings e formatações

--- Primeiros_Passos/entrada_de_dados_f_strings.py
+++ Primeiros_Passos/entrada_de_dados_f_strings.py
@@ -8,3 +8,7 @@ print(nome)  # exibindo o valor digitado
 
 # tambem é possivel fazer a utilização desse valor ja digitado em outro print nesse caso depois a string colocar uma , e o nome da varivel
 print("bem vindo", nome)
+
+# tambem temos a  F string que utilizamos para colocar o nome da variavel junto com as aspas da string e aparecer na tela para o usuario
+
+print(f"bem vindo {nome} ")  # utilizando a f String
---
+++ Primeiros_Passos/output_F_string.py
@@ -0,0 +1,11 @@
+# fazendo output com F string
+# com F string não precisamos fazer a conversão de valores
+
+nome = "Matheus"
+idade = 25
+nick = "M4TC"
+
+x = f"""Meu nome é {nome} minha idade é {idade} e meu nick de jogo é {nick}"""
+print(x)
+
+#------- Formatações com F String -----
