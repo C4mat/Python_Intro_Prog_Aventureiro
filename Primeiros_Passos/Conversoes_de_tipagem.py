925dd858f4e619ae01ad329a93720fdb1939c869
MATheusc4 <74937108+C4mat@users.noreply.github.com>
2025-07-28 21:53:16 -0300

    Aula Conversões de tipagem

---
+++ Primeiros_Passos/Conversoes_de_tipagem.py
@@ -0,0 +1,17 @@
+# podemos realizar conversões tipos de tipagem
+# int() Converte um valor para o tipo inteiro
+# float() Converte um valor para o tipo real
+# str() Converte o valor para uma string
+#  bool() Converte um valor booleano -Verdadeiro ou falso
+
+a = int("1")
+b = str(3)
+c = float('3.15')
+d = bool(0)
+
+print (type(b))
+print(type(a))
+print (type(c))
+print(type(d))
+
+print(d)
