c98053b7805d0024a93f1d906412ef5dcaaafa5b
MATheusc4 <74937108+C4mat@users.noreply.github.com>
2025-07-30 18:16:17 -0300

    Create Operadores_aritimeticos.py

---
+++ Primeiros_Passos/Operadores_aritimeticos.py
@@ -0,0 +1,51 @@
+# ------Opereradores aritimeticos--------
+# + Realiza soma entre operadores ou a concatenação de strings
+# - realiza a subtração das variaveis
+# * realiza a multiplicação das variaveis
+# / realiza a divisão das variaveis
+# // realiza a divisão inteira das variaveis
+# % Retorna o resto da divisão
+# ** Faz a exponeciação
+
+# soma
+num = 5
+num2 = 2
+soma = num + num2
+print("valor da soma", soma)
+
+# subtração
+num = 4
+num2 = 2
+sub = num - num2
+print("valor da subtração", sub)
+
+# Multiplicação
+num = 2
+num2 = 4
+mult = num * num2
+print("esse é o valor a multiplicação", mult)
+
+# divisão
+num = 4
+num2 = 2
+div = num / num2
+print(" Valor da divisão", div)
+
+# divisão inteira
+num = 4
+num2 = 2
+div = num // num2
+print(" Valor da divisão inteira ", div)
+
+# resto da divisão
+num = 5
+num2 = 2
+div = num % num2
+print(" Valor do resto da divisão ", div)
+
+
+# exponenciação(elevado a )
+num = 4
+num2 = 2
+div = num ** num2
+print(" Valor da exponenciação ", div)
